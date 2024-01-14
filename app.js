4911455a65ed0585db1185906ba52052cac38c7d
jaypee15 <okoyejohnpaul15@gmail.com>
2024-01-14 03:32:36 +0100

    feat: server abnd database setup

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const express = require("express")
+const app =express()
+const connectDB = require("./db/connect")
+
+
+// middlewares
+app.use(express.json())
+app.use(express.static("./public"))
+
+const port = process.env.POR || 3000
+
+const start = async ()=>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, ()=>{
+            console.log(`server is litening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
